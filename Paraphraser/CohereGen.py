bc7d73a68e815bce05655e95507daf718be0e895
WafeeRahman <wafeerahman84@gmail.com>
2023-05-06 12:45:48 -0400

    Cohere Model - Toronto Mans

--- Paraphraser/CohereGen.py
+++ Paraphraser/CohereGen.py
@@ -1,10 +1,8 @@
 import cohere
-co = cohere.Client('yourkeyhere')
-
-
-#Test A Prompt From User, Text File
- 
-file = open("input.txt", "r")
-response = co.generate(model = "yourmodelhere", prompt = file.readline())
-file.close()
-print (response)
+co = cohere.Client('CwM1JuOz8DleP9dVvjpcwwM6WnkD4uzaZw0QyCOe') # This is your trial API key
+filei = open("input.txt", "r")
+response = co.generate(
+  model='d04fb84e-1b92-4f0b-934c-eae87f903142-ft',
+  prompt=filei.readline())
+print('Prediction: {}'.format(response.generations[0].text))
+filei.close()
