3c355c31fae069b27a3114ea9a052e133661a0ab
WafeeRahman <wafeerahman84@gmail.com>
2023-05-06 12:22:45 -0400

    DataSet Testing - CSV for Toronto Mans

--- Paraphraser/CohereGen.py
+++ Paraphraser/CohereGen.py
@@ -1,23 +1,16 @@
 import cohere
-co = cohere.Client('ihkXSqNDJRtrS3iR9HxDrWGREDDM4XR3YpbfO2gE')
+co = cohere.Client('yourkeyhere')
 
-#Feed Pickup Line Dataset
 
 #Test A Prompt From User, Text File
  
-file = open("chat.txt", "r")
+file = open("input.txt", "r")
 
 
 response = co.generate(
-    model = "xlarge",
-    prompt = file.readline() + "--",
-    max_tokens= 20,
-    temperature=0.6,
-    k=0,
-    p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop_sequences=["--"],
+    model = "yourmodelhere",
+    prompt = file.readline(),
+ 
 )
 
 print (response)
