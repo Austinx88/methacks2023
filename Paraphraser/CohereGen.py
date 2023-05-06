2ceaa630283e7b8f7ef7258f9c8d18147438d828
WafeeRahman <wafeerahman84@gmail.com>
2023-05-06 16:11:46 -0400

    Toronto - Coheregen

--- Paraphraser/CohereGen.py
+++ Paraphraser/CohereGen.py
@@ -1,14 +1,18 @@
 import cohere
 co = cohere.Client('CwM1JuOz8DleP9dVvjpcwwM6WnkD4uzaZw0QyCOe') # This is your trial API key
-file = open("input.txt", "r")
-prompt1 = file.readline()
-file.close()
+prompt1 = input()
+toronto = """ create a toronto slang pickup line using the given prompt:
+  \nhere are some contraints:
+  \n1. respond with one line
+  \n2. use atleast 2 toronto slang words
+  \n3. only output the conversion and nothing else.
+  \n4. use fam, eh ahlie, nyeah eh, wallahi, and other toronto words to spice up the response\nhere is the given prompt, convert it: """ + prompt1
 response = co.generate(
   model='d04fb84e-1b92-4f0b-934c-eae87f903142-ft',
-  prompt="rephrase the given prompt using toronto slang:\nHere are some constraints:\n1. respond with a one line rephrasement, do not use explicit language \n2. use atleast 2 words of toronto slang\n3. you are not responding to the given prompt\n4. make sure your response is something a torontonian would say\nhere is the given prompt: " + prompt1,
-  max_tokens=300,
+  prompt= toronto,
+  max_tokens=42,
   temperature=1,
-  k=126,
+  k=56,
   stop_sequences=[],
-  return_likelihoods='GENERATION')
-print('Prediction: {}'.format(response.generations[0].text))
+  return_likelihoods='NONE')
+print((response.generations[0].text))
