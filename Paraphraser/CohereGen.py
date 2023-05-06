eb24b6b59bd2ce630c614a6d2096154864e8b92b
WafeeRahman <wafeerahman84@gmail.com>
2023-05-06 14:49:30 -0400

    TorontoMansPromptCreator
    creates prompt using user input stored in fileio, added programmer data set to data set csvs

--- Paraphraser/CohereGen.py
+++ Paraphraser/CohereGen.py
@@ -1,6 +1,14 @@
 import cohere
 co = cohere.Client('CwM1JuOz8DleP9dVvjpcwwM6WnkD4uzaZw0QyCOe') # This is your trial API key
-filei = open("input.txt", "r")
-response = co.generate(model='d04fb84e-1b92-4f0b-934c-eae87f903142-ft', prompt=filei.readline())
+file = open("input.txt", "r")
+prompt1 = file.readline()
+file.close()
+response = co.generate(
+  model='d04fb84e-1b92-4f0b-934c-eae87f903142-ft',
+  prompt="rephrase the given prompt using toronto slang:\nHere are some constraints:\n1. respond with a one line rephrasement, do not use explicit language \n2. use atleast 2 words of toronto slang\n3. you are not responding to the given prompt\n4. make sure your response is something a torontonian would say\nhere is the given prompt: " + prompt1,
+  max_tokens=300,
+  temperature=1,
+  k=126,
+  stop_sequences=[],
+  return_likelihoods='GENERATION')
 print('Prediction: {}'.format(response.generations[0].text))
-filei.close()
