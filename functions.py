f282b1b209fa56c0d84c130b31009fb3fa9c2bf6
shreywy <shreywy@users.noreply.github.com>
2023-05-06 21:35:50 -0400

    updated ask drake fnc

--- functions.py
+++ functions.py
@@ -4,11 +4,11 @@ co = cohere.Client('ihkXSqNDJRtrS3iR9HxDrWGREDDM4XR3YpbfO2gE') # This is your tr
 def drake_generate(prompt):
     response = co.generate(
     model='2d60718a-6428-4d76-bdfa-e7ab09df9ae6-ft',
-    prompt="respond to this as the popular toronto rapper, drake, would. use toronto slang such as wagwan, fam, yo, and yute. this is the prompt:" + prompt,
+    prompt="respond to this as the popular toronto rapper, drake, would. use toronto slang such as these; wagwan=whats up, fam=family/friend, yo, yute=youth, 6ix=toronto. this is the prompt:" + prompt,
     max_tokens=300,
     temperature=0.9,
     k=0,
     stop_sequences=[],
     return_likelihoods='NONE')
-    return ('{}'.format(response.generations[0].text))
+    return response.generations[0].text
     
